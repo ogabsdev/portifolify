2afcdd8355ea0a2b480a30eebb43b9b2e0718529
Gabriel Nadai <wonderworkdev@gmail.com>
2023-12-26 00:47:50 -0300

    chore(devops): fix run

--- application-layer/application/src/main/java/br/com/portifolify/ApplicationStartup.java
+++ application-layer/application/src/main/java/br/com/portifolify/ApplicationStartup.java
@@ -3,9 +3,11 @@ package br.com.portifolify;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApplicationStartup {
+public class ApplicationStartup extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(ApplicationStartup.class);
@@ -13,4 +15,8 @@ public class ApplicationStartup {
         app.run();
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApplicationStartup.class);
+    }
 }
